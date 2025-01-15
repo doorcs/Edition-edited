9f57142b348e28ba9227ad1de5867f7b5ca0cefe
doorcs <castledoor@pusan.ac.kr>
2025-01-15 09:38:28 +0900

    fix: 일부 상황에서 다크모드 적용 시 플리커 현상 제거
    - 다크모드 적용 로직을 `darkmode.hbs`에서 `main.js`로 이동
    - 이제 `darkmode.hbs`에서는 다크모드 토글 버튼의 현재 상태를 결정하고, Event Listener를 추가해주는 동작만 수행
    - getCookie() 함수 선언을 inlining
    - 자바스크립트의 hoisting을 활용해 코드 가독성 개선
    - preference 분기 처리 로직을 개선

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,19 @@ var body = document.body;
 var timeout;
 var st = 0;
 
+checkCookie(); // Javascrip의 호이스팅(Hoisting)을 활용! ( 함수 선언문은 자동으로 상단에 끌어올려지므로, 함수 선언 이전에도 호출 가능 )
 cover();
 featured();
 pagination(false);
+
+document.addEventListener('DOMContentLoaded', function () { // 하이퍼링크를 현재 창 대신 새 창에서 열기
+    var links = document.querySelectorAll('a');
+    links.forEach(function (link) {
+        if (link.hostname != window.location.hostname) {
+            link.target = '_blank';
+        }
+    });
+});
 
 window.addEventListener('scroll', function () {
     'use strict';
@@ -75,11 +85,25 @@ function featured() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.querySelectorAll('a');
-    links.forEach(function (link) {
-        if (link.hostname != window.location.hostname) {
-            link.target = '_blank';
+function checkCookie() { // 다크모드 적용 로직
+    let ca = document.cookie.split(';');
+    let preference = "none";
+
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf("darkmode-preferred=") === 0) {
+            preference = c.substring("darkmode-preferred=".length, c.length);
+            break;
         }
-    });
-});
+    }
+
+    if (preference === "true") {
+        body.classList.add("dark");
+    } else if (preference === "false") {
+        body.classList.remove("dark");
+    } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+        body.classList.add("dark");
+    } else {
+        body.classList.remove("dark");
+    }
+}
